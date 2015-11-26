c8699d8df5bec398b622d3cac2cc846e1ceb5d59
Gene Hsu <gene.hsu@utexas.edu>
2015-11-26 14:46:30 -0600

    Map and corporate submission form and schedule
    Fixed map to include both locations.
    Fixed corporate form to include company and correct packages. Fixed
    schedule for day 3

--- assets/js/init.js
+++ assets/js/init.js
@@ -201,71 +201,6 @@ jQuery( document ).ready(function( $ ) {
 		
 
 	/** Registration Form for students**/
-	$("#submit_btn_students").click(function() 
-	{ 
-
-		var filter = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-		
-		var e = document.getElementById("field_1");
-		var user_type = e.options[e.selectedIndex].text;
-		
-	
-		var person_name = document.getElementById("name").value;
-		var user_email = document.getElementById("email1").value;
-		
-		var proceed = true;
-		
-		
-		if(person_name == ""){ 
-			var error1 = '<div class="enter-name col-lg-3 align-center"> Enter the name </div>';
-			jQuery("#result").hide().html(error1).fadeIn(500);
-			proceed = false;
-			return false;
-		}
-		
-		if(user_email== ""){
-			var error2 = '<div class="enter-email col-lg-3 align-center"> Enter the email </div>';
-			jQuery("#result").hide().html(error2).fadeIn(500);
-			proceed = false;
-			
-		}
-		else if(!filter.test(user_email)) {
-			var invalid = '<div class="invalid-email col-lg-3 align-center"> Invalid Email </div>';
-			jQuery("#result").hide().html(invalid).fadeIn(500);
-			proceed = false;
-			
-		}
-	
-		if (proceed) //everything looks good! proceed...
-		{
-			
-			//data to be sent to server
-			var post_data = {
-				'userName': person_name,
-				'userType': user_type,
-				'userEmail': user_email,
-
-			}
-			
-			//Ajax post data to server
-			jQuery.post('contact_me_students.php', post_data, function(response) {
-				//load json data from server and output message
-				if (response.type == 'error') {
-					var output = '<div class="error col-lg-3 align-center">' + response.text + '</div>';
-				} else {
-					var output = '<div class="success col-lg-3 align-center">' + response.text + '</div>';
-					//reset values in all input fields
-					
-				}
-				jQuery("#result").hide().html(output).fadeIn(500);
-				
-			}, 'json');
-		}
-
-		return false;
- 
-	});
-    
     $("#submit_btn_corporate").click(function() 
 	{ 
 
@@ -276,6 +211,7 @@ jQuery( document ).ready(function( $ ) {
 		
 	
 		var person_name = document.getElementById("name").value;
+        var person_company = document.getElementById("company").value;
 		var user_email = document.getElementById("email1").value;
 		
 		var proceed = true;
@@ -283,6 +219,12 @@ jQuery( document ).ready(function( $ ) {
 		
 		if(person_name == ""){ 
 			var error1 = '<div class="enter-name col-lg-3 align-center"> Enter the name </div>';
+			jQuery("#result").hide().html(error1).fadeIn(500);
+			proceed = false;
+			return false;
+		}
+        if(person_company == ""){ 
+			var error1 = '<div class="enter-name col-lg-3 align-center"> Enter the company </div>';
 			jQuery("#result").hide().html(error1).fadeIn(500);
 			proceed = false;
 			return false;
@@ -307,6 +249,7 @@ jQuery( document ).ready(function( $ ) {
 			//data to be sent to server
 			var post_data = {
 				'userName': person_name,
+                'userCompany': person_company,
 				'userType': user_type,
 				'userEmail': user_email,
 
